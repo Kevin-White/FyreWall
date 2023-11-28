2e9043480d36d5af03c577ecea59c06e9a03c0fb
Kevin White <58002977+Kevin-White@users.noreply.github.com>
2023-11-27 20:02:58 -0600

    Finishing touces

--- src/LevelSelect.java
+++ src/LevelSelect.java
@@ -5,8 +5,6 @@ import javax.swing.SwingUtilities;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
